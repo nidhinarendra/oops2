4073c1f1055d26f0f4e538631dd9f895ec4cadef
stupidfellow <nidhi.narendra1@gmail.com>
2016-03-12 21:01:36 -0800

    initial commit

---
+++ arguments.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//class vector 
+//{
+//vector 
+
+int main (int argc, char *argv[])
+{
+
+
+//cout << argc << endl << *argv << endl;
+//cout << argc << endl << argv[0]<< endl<< argv[1] << endl<< argv[2] << argv[3]<<endl; //both gives the same output
+
+//char size = vectorsize(**argv);
+//cout << "size of the vector is " <<size << endl;
+return 0;
+
+}
