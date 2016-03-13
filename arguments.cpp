415b3214fff28b481efaf00d1b2323a46bb32fe9
stupidfellow <nidhi.narendra1@gmail.com>
2016-03-13 15:49:38 -0700

    class created

--- arguments.cpp
+++ arguments.cpp
@@ -2,19 +2,75 @@
 
 using namespace std;
 
-//class vector 
-//{
-//vector 
+class vector 
+{
+int length_of_array;
+int* contents;
+
+vector (int in_length)
+{
+contents = new int[in_length];
+for(int i = 0; i<=in_length; i++)
+ {
+ contents[i] = i;
+ }
+
+length_of_array = in_length;
+}
+
+~vector()
+{
+delete[] length_of_array;
+length_of_array = 0;
+}
+
 
 int main (int argc, char *argv[])
 {
+int length_of_array=0;
+char* argument = NULL;
+bool shallow = false;
+bool deep = false;
+bool copy = false;
+bool reverse = false;
+
+for (int i=0; i<=argc; i++)
+{
+argument = argv[i];
+switch (argument)
+{
+case "-s": 
+length_of_array = argv[i+1];
+break;
+
+case "-S": //Shallow copy
+shallow = true;
+break;
+
+case "-D": //Deep copy
+deep = true;
+break;
+
+case "-C": //copy previous vector to current vector
+copy = true;
+break;
+
+case "-R"://reverse the content of the current vector
+reverse = true;
+break;
+
+case "-O": //ouput the vector
+if (char a = 'p')
+????
+break;
+
+default :
+cerr << "Unknown argument\n";
+}
+}
 
 
-//cout << argc << endl << *argv << endl;
-//cout << argc << endl << argv[0]<< endl<< argv[1] << endl<< argv[2] << argv[3]<<endl; //both gives the same output
 
-//char size = vectorsize(**argv);
-//cout << "size of the vector is " <<size << endl;
 return 0;
 
 }
