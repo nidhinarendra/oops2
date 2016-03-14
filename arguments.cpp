95518db67c1f40d5cf8ecc4a072b94044f386157
stupidfellow <nidhi.narendra1@gmail.com>
2016-03-14 00:37:12 -0700

    Added reverse logic

--- arguments.cpp
+++ arguments.cpp
@@ -5,94 +5,109 @@ using namespace std;
 class Vector 
 {
 private:
-int length_of_array;
-int* contents;
+  int length_of_array;
+  int* contents;
 
 public:
-Vector (int in_length)
-{
-contents = new int[in_length];
-for(int i = 0; i<=in_length; i++)
- {
- contents[i] = i;
- }
+  Vector (int in_length)
+  {
+    contents = new int[in_length];
+    for(int i = 0; i<=in_length; i++)
+      {
+	contents[i] = i;
+      }
 
-length_of_array = in_length;
-}
+    length_of_array = in_length;
+  }
 
-~Vector()
-{
-delete[] length_of_array;
-length_of_array = 0;
-}
+  ~Vector()
+  {
+    delete[] length_of_array;
+    length_of_array = 0;
+  }
+
+  Vector::Reverse (const Vector& obj1)
+  {
+    for(int i=0; i<length_of_array-1; i++)
+      {
+	obj1.contents[i] = obj1.contents[length_of_array - i - 1];
+      }
+
+  }
+
 
 };
 
 class ChildVector : public Vector
 {
-ChildVector::ChildVector(const ChildVector & Source)
-{
-length_of_array = Source.length_of_array; //Shallow copy as length_of_array is not a pointer
+  ChildVector::ChildVector(const ChildVector & Source)
+  { //Shallow copy as length_of_array is not a pointer
+    length_of_array = Source.length_of_array;
 
-if (Source.contents)
-{
-contents = new int[length_of_array];
-void *memcpy ((void*) contents , const (void*) Source.contents, length_of_array)
-}
-else contents = 0;
-}
-}
-
-
-
+    if (Source.contents)
+      {
+	contents = new int[length_of_array];
+	void *memcpy ((void*) contents , const (void*) Source.contents,
+		      length_of_array)
+	  }
+    else contents = 0;
+  }
 };
+	
+ChildVector& ChildVector:: operator >> ()
+{
+  for (int i = 0; i<= length_of_array; i++)
+    {
+      cout << contents[i];
+    }
+}
+
 
 int main (int argc, char *argv[])
 {
-int length_of_array=0;
-char* argument = NULL;
-bool shallow = false;
-bool deep = false;
-bool copy = false;
-bool reverse = false;
+  int length_of_array=0;
+  char* argument = NULL;
+  bool shallow = false;
+  bool deep = false;
+  bool copy = false;
+  bool reverse = false;
 
-for (int i=0; i<=argc; i++)
-{
-argument = argv[i];
-switch (argument)
-{
-case "-s": 
-length_of_array = argv[i+1];
-break;
+  for (int i=0; i<=argc; i++)
+    {
+      argument = argv[i];
+      switch (argument)
+	{
+	case "-s": 
+	  length_of_array = argv[i+1];
+	  break;
 
-case "-S": //Shallow copy
-shallow = true;
-break;
+	case "-S": //Shallow copy
+	  shallow = true;
+	  break;
 
-case "-D": //Deep copy
-deep = true;
-break;
+	case "-D": //Deep copy
+	  deep = true;
+	  break;
 
-case "-C": //copy previous vector to current vector
-copy = true;
-break;
+	case "-C": //copy previous vector to current vector
+	  copy = true;
+	  break;
 
-case "-R"://reverse the content of the current vector
-reverse = true;
-break;
+	case "-R"://reverse the content of the current vector
+	  Vector obj1;
+	  obj1.Reverse(obj1)
+	    reverse = true;
+	  break;
 
-case "-O": //ouput the vector
-if (char a = 'p')
-????
-break;
+	case "-O": //ouput the vector
+	  if (char a = 'p')
+	    ????
+	    break;
 
-default :
-cerr << "Unknown argument\n";
-}
-}
+	default :
+	  cerr << "Unknown argument\n";
+	}
+    }
 
-
-
-return 0;
-
+  return 0;
 }
