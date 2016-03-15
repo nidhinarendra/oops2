b48fc9140cd3253b656f694ce94847afdb43abd8
ndarshanrestless <darshan@restlessbandit.com>
2016-03-15 06:39:08 +0000

    some what working

--- test.cpp
+++ test.cpp
@@ -13,47 +13,59 @@ public:
   {
     length_vector_array = length;
     contents = new int[length];
-    cout << "Length of array is " << length_vector_array <<endl;
-  }
 
-  int Create_array()
-  {
     for (int i=0; i<=length_vector_array-1;i++)
       {
 	contents[i] = i;
-	cout << "content[" <<i << "]"<< "is" << contents[i] <<endl;
+	//cout << "contents[" <<i << "]"<< "is" << contents[i] <<endl;
       }
-    return *contents;
   }
-  int deep_copy()
+  int reverse()
   {
-    cout << "Performing deep copy\n";
-    return 0;
-  }
-
-  int Copy (int length_array)
-  {
-    cout << "Assignment operation\n";
-    return 0;
-  }
-
-  int Reverse(int length_vector_array)
-  {
-    for (int i=0; i<length_vector_array; i++)
+    int temp;
+    int middle;
+    if ((length_vector_array % 2) == 0)
+      middle = length_vector_array/2;
+    else
+      middle = ((int)length_vector_array/2) - 1;
+	
+    for (int i=0; i < middle; i++)
       {
-	contents[i] = contents[length_vector_array - i -1];
+	temp = contents[length_vector_array - i -1];
+	contents[length_vector_array - i - 1] = contents[i];
+	contents[i] = temp;
 	cout << "Reverse" << contents[i] << endl;
       }
     return *contents;
   }
+  
+  void print()
+  {
+    for (int i=0; i<=length_vector_array-1;i++)
+      {
+	cout << "content[" <<i << "]"<< "is" << contents[i] <<endl;
+      }
+  }
 
+  /*
+  istream & operator>>(istream &in)//const Vectorclass &obj)
+  {
+    for (int i=0; i<=length_vector_array-1;i++)
+      {
+	in>>contents[i];
+	//cout << "content[" <<i << "]"<< "is" << contents[i] <<endl;
+      }
+    return in;
+  }
+  */
+  
 };
 
 class ChildVectorclass : public Vectorclass
 {
+public:
   ChildVectorclass(const Vectorclass & Source) : Vectorclass(Source)
   {
-    cout << "Entreing shallow copy" << endl;
     length_vector_array = Source.length_vector_array;
     if(Source.contents)
       {
@@ -63,6 +75,7 @@ class ChildVectorclass : public Vectorclass
       }
     else contents = 0;
   }
+  
 };
 
 
@@ -70,49 +83,71 @@ int main (int argc, char* argv[])
 {
   int length_of_array = 0;
   char * argument = NULL;
-  Vectorclass *cur;
-  Vectorclass *prev;
+  Vectorclass *cur = NULL;
+  Vectorclass *prev = NULL;
   for (int i=1; i<argc; i++)
     {
       argument = argv[i];
       if(strcmp((const char *)argument, (const char *) "-s") == 0)
 	{
+	  cout << "Inside -s\n"; 
 	  length_of_array = atoi((const char *) argv[i+1]);
 	  Vectorclass obj1(length_of_array);
-	  obj1.Create_array();
 	  cur = &obj1;
+	  (*cur).print();
+	  i++;
+	  //cout>>cur;
 	}
        else if(strcmp((const char *)argument,(const char *) "-S") == 0)
 	{
-	  cout << "array lenght is " << length_of_array<<endl;
+	  cout << "Inside -S\n"; 
 	  prev = cur;
 	  Vectorclass obj2 = *prev;	 
 	  cur = &obj2;
-	 }
 
+	  (*prev).print();
+	  (*cur).print();
+	}
       else if(strcmp((const char *)argument, (const char *) "-D") == 0)
 	{
+	  cout << "Inside -D\n"; 
 	  prev = cur;
-	  Vectorclass obj1(length_of_array);
-	  //ChildVectorclass obj3(length_of_array);
-	  //obj3.deep_copy();
-	}
+	  ChildVectorclass obj3 = *cur;
+	  cur = &obj3;
 
+	  (*prev).print();
+	  cout << &(*prev).contents <<"\n";
+		  
+	  (*cur).print();
+	  cout << &(*cur).contents <<"\n"; 
+
+	}
       else if(strcmp ((const char *) argument, (const char*) "-C") == 0)
 	{
-	  Vectorclass obj4(length_of_array);
-	  obj4.Copy(length_of_array);
+	  cout << "Inside -C\n"; 
+	  prev = cur;
+	  Vectorclass obj4 = *cur;
 	}
       else if(strcmp ((const char *) argument, (const char*) "-R") == 0)
 	{
-	  Vectorclass obj5(length_of_array);
-	  obj5.Reverse(length_of_array);
-	  break;
-	}
+	  cout << "Inside -R\n"; 
+	  if(cur) 
+	    (*cur).reverse();
+	  else
+	    cerr << "current vector is NULL";
+	  
+	  (*prev).print();
+	  cout << &(*prev).contents <<"\n";
+		  
+	  (*cur).print();
+	  cout << &(*cur).contents <<"\n"; 
+
+
+ 	}
       else 
 	{cout << "unknown argument" << argv[i];}
     }
-
+  cout << "End of processing"; 
 
   return 0;
 
